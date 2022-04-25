6f3142447fcad1636c6d92c1cc6ba621f9549eb0
arieleandre <arielebrites15@gmail.com>
2022-04-25 20:30:58 -0300

    primeiro commit

---
+++ login.js
@@ -0,0 +1,9 @@
+function mudarCor(){
+ var cor = document.querySelector('.button')
+ cor.style.backgroundColor = "#733e30"
+}
+
+function voltarCor(){
+    var cor = document.querySelector('.button')
+    cor.style.backgroundColor = "rgb(148, 95, 81)" 
+}
